56ae15797c3d7d09136f4d5126667b8dfe7dba7c
harrysandyryan01 <92442490+harrysandyryan01@users.noreply.github.com>
2021-10-28 20:24:45 +0700

    Add files via upload

---
+++ 02 tugas/TugasLibraryUnitTest/Library.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    public class Library
+    {
+        private Library library;
+
+        [SetUp]
+        public void Init()
+        {
+            library = new Library();
+        }
+
+        [Test]
+        public void DayTest()
+        {
+            Console.WriteLine("Senin");
+            Console.WriteLine("Selasa");
+            Console.WriteLine("Rabu");
+            Console.WriteLine("Kamis");
+            Console.WriteLine("Jumat");
+            Console.WriteLine("Sabtu");
+            Console.WriteLine("Minggu");
+        }
+
+        [Test]
+        public void UserTest()
+        {
+            Console.WriteLine("Password = admin");
+        }
+    }
+}
